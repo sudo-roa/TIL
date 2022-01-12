28e096109e23de17fd23f1f91fc0fc671602912a
sudo-roa <sudo.roa9393@gmail.com>
2022-01-13 03:19:27 +0900

    update

--- plantUML/MindMap/Github_TIL/updateRepoSt.sh
+++ plantUML/MindMap/Github_TIL/updateRepoSt.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+case `uname -s` in
+    Linux)
+        echo 'OSはLinuxです。macで実行してください。'
+        exit;;
+    Darwin)
+        echo 'plantumlを実行します。'
+        break;;
+    *)
+        echo 'OSがわかりません'
+        exit;;
+esac
 
 plantuml RepositoryStructure.puml &&\
 plantuml RepositoryStructureJ.puml &&\
